6dd6a897f2922caa9426d097f09c40399ca6d2b0
Danny Sandi <dasandiro@gmail.com>
2016-07-22 21:24:58 -0600

    ShellHandler Implementation

--- GpxProcessor/Program.cs
+++ GpxProcessor/Program.cs
@@ -10,6 +10,14 @@ namespace GpxProcessor
     {
         static void Main(string[] args)
         {
+            var shell = new ShellHandler("\"C:\\Program Files\\Genymobile\\Genymotion\\genyshell.exe\"")
+            {
+                Modifier = "-c"
+            };
+
+            var output = shell.SetLatitude(87);
+            Console.Write(output);
+            Console.ReadLine();
         }
     }
 }
--- GpxProcessor/ShellHandler.cs
+++ GpxProcessor/ShellHandler.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,22 +10,56 @@ namespace GpxProcessor
     public class ShellHandler
     {
         private string _shellPath;
-        private string _modifier;
+        private Process _cmd;
+        private bool _running;
+
+        private const string LatCmd = "\"gps setlatitude \"";
+        private const string LonCmd = "\"gps setlongitude \"";
+
+
+        public string Modifier { get; set; }
 
         public ShellHandler(string path)
         {
             _shellPath = path;
+            _running = false;
             _init();
         }
 
         private void _init()
         {
-            
+            _cmd = new Process
+            {
+                StartInfo =
+                {
+                    FileName = "cmd.exe",
+                    RedirectStandardInput = true,
+                    RedirectStandardOutput = true,
+                    UseShellExecute = false
+                }
+            };
+            _cmd.Start();
+            _running = true;
         }
 
-        public void ExecuteCommand()
+        private string ExecuteCommand(string command)
         {
-            
+            if(!_running)
+                throw new Exception("The shell is not running");
+            _cmd.StandardInput.WriteLine(string.Join(" ", _shellPath, Modifier, command));
+            _cmd.StandardInput.Flush();
+            _cmd.StandardInput.Close();
+            return _cmd.StandardOutput.ReadToEnd();
+        }
+
+        public string SetLatitude(int latitude)
+        {
+            return ExecuteCommand(LatCmd + latitude);
+        }
+
+        public string SetLongitude(int longitude)
+        {
+            return ExecuteCommand(LonCmd + longitude);
         }
     }
 }
