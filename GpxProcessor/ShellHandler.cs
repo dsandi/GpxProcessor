70c3caf78da312667f6a2c14fb5ee6b288218055
Danny Sandi <dasandiro@gmail.com>
2016-07-22 19:53:51 -0600

    Base

---
+++ GpxProcessor/ShellHandler.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GpxProcessor
+{
+    public class ShellHandler
+    {
+        private string _shellPath;
+        private string _modifier;
+
+        public ShellHandler(string path)
+        {
+            _shellPath = path;
+            _init();
+        }
+
+        private void _init()
+        {
+            
+        }
+
+        public void ExecuteCommand()
+        {
+            
+        }
+    }
+}
